98fc7a5b2ab8a445ff1eed27fcee99524985f8f3
James Weakley <jameswillisweakley@gmail.com>
2020-03-05 09:25:28 +1100

    Initial

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
-print("Server running")
+import numpy as np
+from numba import njit
+import ray
+
+@njit(fastmath=True)
+def mz_centroid(_int_f, _mz_f):
+    return ((_int_f/_int_f.sum()) * _mz_f).sum()
+
+
+@ray.remote
+def f():
+    a = np.random.random(10)
+    b = 123
+    return mz_centroid(a, b)
+
+
+ray.init(address='auto', redis_password='5241590000000000')
+results = ray.get([f.remote() for i in range(4)])
+print(results)
